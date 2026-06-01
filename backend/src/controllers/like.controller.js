d4b991dd3663b392fc2b7a125391dd8b5f332d06
Aftab <muhammadaftab584@gmail.com>
2026-06-01 18:42:23 +0500

    like controller update

--- backend/src/controllers/like.controller.js
+++ backend/src/controllers/like.controller.js
@@ -22,12 +22,32 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const { commentId } = req.params
     //TODO: toggle like on comment
+    const { userId } = req.user._id
+    //TODO: toggle like on video
+    const existingLike = await Like.findById({ comment: commentId, likedBy: userId })
+    let res;
+    if (existingLike) {
+        res = await Like.findByIdAndDelete({ _id: existingLike._id })
+    } else {
+        res = await Like.create({ comment: commentId, likedBy: userId })
+    }
+    return res.status(200).json(new ApiResponse(200, null, "Like Toggle Successfull"))
 
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const { tweetId } = req.params
     //TODO: toggle like on tweet
+    const { userId } = req.user._id
+    //TODO: toggle like on video
+    const existingLike = await Like.findById({ tweet: tweetId, likedBy: userId })
+    let res;
+    if (existingLike) {
+        res = await Like.findByIdAndDelete({ _id: existingLike._id })
+    } else {
+        res = await Like.create({ tweet: tweetId, likedBy: userId })
+    }
+    return res.status(200).json(new ApiResponse(200, null, "Like Toggle Successfull"))
 }
 )
 
@@ -35,7 +55,11 @@ const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
     const { user } = req;
 
-    await Like.$where({ likedBy: user._id })
+    const likedVideos = await Like.find({ likedBy: user._id })
+
+    if (!likedVideos || likedVideos.length === 0) {
+        return res.status(200).json(new ApiResponse(200, { length: 0 }, "No videos liked."))
+    }
 })
 
 export {
